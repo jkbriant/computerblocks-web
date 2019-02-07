9072f5f09c8f960b6f8de2177b99ef8134fb686e
Joshua Briant <jkbriant@gmail.com>
2019-02-07 19:27:54 +1100

    contentish

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/about.html'));
+});
+
 app.listen(8080);
+// app.listen(process.env.PORT || 8080);
