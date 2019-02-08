1bdf1693683590a190e39d43f34eb4a785d6977a
Joshua Briant <jkbriant@gmail.com>
2019-02-09 10:22:45 +1100

    Lots of content and images

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'))
 });
