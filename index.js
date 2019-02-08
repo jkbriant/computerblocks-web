4d2621d5ec7f428917f3123c4a1a0d90788d32eb
Joshua Briant <jkbriant@gmail.com>
2019-02-08 19:18:41 +1100

    cleaning and actually pushing the gooooogle stuff

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+var express = require('express')
+var app = express()
+var path = require('path')
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
 app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/about.html'));
-});
+    res.sendFile(path.join(__dirname + '/views/about.html'))
+})
 
-app.listen(8080);
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 8080)
